eb70275fefd94cac8af8edf48b4cdc198c855bcd
Josue Tapia <47112169+josuetsm@users.noreply.github.com>
2026-01-26 04:13:51 -0300

    Add Gaia auth helpers and name resolution

---
+++ app_unzipped/gaia_cache.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from gaia_cache import load_gaia_auth, resolve_name_to_icrs, save_gaia_auth
+
+__all__ = ["load_gaia_auth", "save_gaia_auth", "resolve_name_to_icrs"]
